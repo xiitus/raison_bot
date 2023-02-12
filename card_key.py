cc59de7ad9d0cceec5fd745c45ed83eb9bd59fd6
jaydolphXX <justinwantsomemo@gmail.com>
2023-02-12 12:03:01 +0900

    second commit

---
+++ card_key.py
@@ -0,0 +1,66 @@
+import os
+from dotenv import load_dotenv
+from discord import Intents, Client
+
+load_dotenv()
+
+token = os.environ["CARD_BOT_TOKEN"]
+guild_id = int(os.environ["GUILD_ID"])
+bot_channel_id = int(os.environ["BOT_CHANNEL_ID"])
+card_role_id = int(os.environ["CARD_2F_ROLE_ID"])
+card_2f_channel_id = int(os.environ["CARD_2F_CHANNEL_ID"])
+
+intents = Intents.default()
+intents.members = True
+intents.message_content = True
+intents.presences = True
+
+client = Client(intents=intents)
+
+card_can_take = True
+
+@client.event
+async def on_ready():
+    print(f"Cardey が起動しました")
+
+@client.event
+async def on_message(message):
+    global card_can_take
+
+    is_bot_channel = (message.channel.id == bot_channel_id)
+    is_2f_cardkey_channel = (message.channel.id == card_2f_channel_id)
+    role = message.guild.get_role(card_role_id)
+
+    takelike_words = {"take", "ｔａｋｅ", "たけ", "タケ", "ﾀｹ", "rake", "竹", "ていく", "テイク", "ﾃｲｸ", "teiku", "ｔｅｉｋｕ"}
+    returnlike_words = {"return", "ｒｅｔｕｒｎ", "れつrn", "れつｒｎ", "teturn", "returm", "リターン", "りたーん", "ﾘﾀｰﾝ", "列rn"}
+    helplike_words = {"help", "ｈｅｌｐ", "へlp", "ヘｌｐ", "へるぷ", "ヘルプ", "たすけて", "ﾍﾙﾌﾟ", "助けて", "ﾀｽｹﾃ", 
+                        "タスケテ", "ﾍlp", "hwkp", "hekp", "jelp", "felp", "gelp"}
+
+    if (is_bot_channel):
+        user_said = message.content.lower()
+        if (message.author.bot):
+            return
+
+        if (user_said in helplike_words):
+            await message.channel.send(f"***help***     コマンド -> このメッセージ(**self, this**)を表示\n***take***     コマンド -> カードキーを所持していることを示すロールを付与\n***return*** コマンド -> カードキー返却時に、takeコマンドで付与したロールを剥奪")
+
+        if (user_said in takelike_words):
+            if (card_can_take == True):
+                card_can_take = False
+                await message.channel.send(f"**<@{message.author.id}> がカードキーを装備しました!**")
+                await message.author.add_roles(role)
+            elif (card_can_take == False):
+                role_member = role.members[0].id
+                await message.channel.send(f"**カードは<@{role_member}> が装備中です!**")
+
+
+        if (user_said in returnlike_words):
+            if (card_can_take == True):
+                await message.channel.send(f"**カードはまだ 2F にあります!**")
+            elif (card_can_take == False):
+                card_can_take = True
+                await message.channel.send(f"**<@{message.author.id}> がカードキーを返却しました!**")
+                await message.author.remove_roles(role)
+
+client.run(token)
+
